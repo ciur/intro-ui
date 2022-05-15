a0e05a94c2e2b1f8bd7c43cf043b2f7c87f7db32
Eugen Ciur <eugen@papermerge.com>
2022-05-15 09:35:37 +0200

    first commit

--- app/app.js
+++ app/app.js
@@ -2,6 +2,8 @@ import Application from '@ember/application';
 import Resolver from 'ember-resolver';
 import loadInitializers from 'ember-load-initializers';
 import config from 'intro-ui/config/environment';
+import '@popperjs/core';
+import 'bootstrap';
 
 export default class App extends Application {
   modulePrefix = config.modulePrefix;
